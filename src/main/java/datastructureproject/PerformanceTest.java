ac23363ed85871edc27ad7349c008575891ab378
Joel <joel.pietilainen@helsinki.fi>
2023-11-20 18:37:44 +0200

    link

--- src/main/java/datastructureproject/PerformanceTest.java
+++ src/main/java/datastructureproject/PerformanceTest.java
@@ -25,6 +25,8 @@ public class PerformanceTest {
         
         PerformanceTest pt = new PerformanceTest();
 
+        //https://www.chessprogramming.org/Perft_Results for test positions and results
+
         pt.perft("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 4, false);
         
     }
